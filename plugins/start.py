8e784233b3d8d5581af46400bac879cb9f2da370
MINDHELL <Tgpurpose640@gmail.com>
2025-06-30 14:21:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -189,10 +189,10 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(text="Join Channel", url=client.invitelink),
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+            #InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
         ],
         [
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
+            #InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
             #InlineKeyboardButton(text="Join Channel", url=client.invitelink4),
         ]
     ]
